c25fe94ad535ec8ae21d68e4982e2349c09ddd3c
CodeJobToey <prapanpong.job@gmail.com>
2023-04-27 16:35:45 +0700

    project-403pkw04-caregiver

--- lib/main.dart
+++ lib/main.dart
@@ -39,80 +39,126 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => MyMapPageState();
 }
 
+// class MyMapPageState extends State<MyHomePage> {
+//   final Completer<GoogleMapController> _controllerss = Completer();
+//   final Set<Circle> _circles = {}; // Set to store circles on the map
+//   final LatLng _center = const LatLng(13.928912169164503, 100.57660708828934);
+//   final double _zoom = 16.0;
+//   late LocationData currentLocation;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: GoogleMap(
+//         //ขออนุญาตให้เข้าดู location ของเราได้ - เจ้าของบ้านจะอนุญาตให้เข้าถึงตำแหน่งหรือเปล่า?
+//         myLocationEnabled: true,
+//         // กำหนดให้ แผนที่แสดงออกมาในรูปแบบ normal
+//         mapType: MapType.normal,
+//         // คำสั่งให้สร้างแผนที่ขึ้นมา
+//         onMapCreated: (GoogleMapController controller) {
+//           _controllerss.complete(controller);
+//         },
+//         // กำหนดตำแหน่งเริ่มต้นให้กับแอปพลิเคชัน โดยตำแหน่งเริ่มต้นอยู่ที่อนุสาวรีย์ มีการซูมอยู่ที่ 16
+//         initialCameraPosition: CameraPosition(
+//           target: _center,
+//           zoom: _zoom,
+//         ),
+//         circles: _circles,
+//       ),
+
+//       // สร้างปุ่มขึ้นมา โดยมีชื่อว่า My location
+//       floatingActionButton: FloatingActionButton.extended(
+//         // เมื่อกดปุ่ม จะรับค่าตำแหน่งของอุปกรณ์ ส่งผ่านเข้าไปในตัวแปร currentLocation และไปยังฟังก์ชัน _goToMyLocation() และ _saveMapData();
+//         onPressed: () async {
+//           currentLocation = await Location().getLocation();
+
+//           _goToMyLocation();
+//           _saveMapData();
+
+//           setState(() {
+//             _circles.add(
+//               Circle(
+//                 circleId: const CircleId('home'), // Unique ID for the circle
+//                 center: LatLng(
+//                     currentLocation.latitude!, currentLocation.longitude!),
+//                 // Home location (latitude, longitude)
+//                 radius: 100, // Radius in meters
+//                 strokeWidth: 2, // Width of the circle's stroke
+//                 strokeColor: Colors.blue, // Color of the circle's stroke
+//                 fillColor:
+//                     Colors.blue.withOpacity(0.2), // Fill color of the circle
+//               ),
+//             );
+//           });
+//         },
+//         label: const Text('My location'),
+//         icon: const Icon(Icons.near_me),
+//       ),
+//     );
+//   }
+
+//   //ฟังก์ชัน _goToMyLocation() มีหน้าที่ หาตำแหน่งใหม่จากได้ที่กำหนดเริ่มต้น เป็นตำแหน่ง ณ ปัจจุบัน
+//   Future _goToMyLocation() async {
+//     final controller = await _controllerss.future;
+//     controller.animateCamera(
+//       CameraUpdate.newCameraPosition(
+//         CameraPosition(
+//           target: LatLng(currentLocation.latitude!, currentLocation.longitude!),
+//           zoom: 16,
+//         ),
+//       ),
+//     );
+//   }
+
+//   //ฟังก์ชัน _saveMapData() มีหน้าที่ เก็บตำแหน่ง ณ ปัจจุบัน ไปไว้ที่ Firebase
+//   void _saveMapData() async {
+//     var mapData = {
+//       "latitude": currentLocation.latitude,
+//       "longitude": currentLocation.longitude,
+//     };
+
+//     // Timer เป็นการกำหนดเวลาให้ส่งค่า latitude และ longitude ทุก ๆ _ วินาที
+//     Timer.periodic(const Duration(seconds: 3), (timer) {
+//       // print(currentLocation);
+//       FirebaseFirestore.instance
+//           .collection("maps2")
+//           .doc("LatLng")
+//           // .doc()
+//           .set(mapData)
+//           .catchError((error) {
+//         print("Error saving map data: $error");
+//       });
+//     });
+//   }
+// }
+
 class MyMapPageState extends State<MyHomePage> {
-  final Completer<GoogleMapController> _controllerss = Completer();
-
-  late LocationData currentLocation;
-
-  final LatLng _center = const LatLng(13.928912169164503, 100.57660708828934);
-  final double _zoom = 16.0;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: GoogleMap(
-        //ขออนุญาตให้เข้าดู location ของเราได้ - เจ้าของบ้านจะอนุญาตให้เข้าถึงตำแหน่งหรือเปล่า?
-        myLocationEnabled: true,
-        // กำหนดให้ แผนที่แสดงออกมาในรูปแบบ normal
-        mapType: MapType.normal,
-        // คำสั่งให้สร้างแผนที่ขึ้นมา
-        onMapCreated: (GoogleMapController controller) {
-          _controllerss.complete(controller);
-        },
-        // กำหนดตำแหน่งเริ่มต้นให้กับแอปพลิเคชัน โดยตำแหน่งเริ่มต้นอยู่ที่อนุสาวรีย์ มีการซูมอยู่ที่ 16
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: _zoom,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(widget.title),
         ),
-      ),
-
-      // สร้างปุ่มขึ้นมา โดยมีชื่อว่า My location
-      floatingActionButton: FloatingActionButton.extended(
-        // เมื่อกดปุ่ม จะรับค่าตำแหน่งของอุปกรณ์ ส่งผ่านเข้าไปในตัวแปร currentLocation และไปยังฟังก์ชัน _goToMyLocation() และ _saveMapData();
-        onPressed: () async {
-          currentLocation = await Location().getLocation();
-
-          _goToMyLocation();
-          _saveMapData();
-        },
-        label: const Text('My location'),
-        icon: const Icon(Icons.near_me),
-      ),
-    );
-  }
-
-  //ฟังก์ชัน _goToMyLocation() มีหน้าที่ หาตำแหน่งใหม่จากได้ที่กำหนดเริ่มต้น เป็นตำแหน่ง ณ ปัจจุบัน
-  Future _goToMyLocation() async {
-    final controller = await _controllerss.future;
-    controller.animateCamera(
-      CameraUpdate.newCameraPosition(
-        CameraPosition(
-          target: LatLng(currentLocation.latitude!, currentLocation.longitude!),
-          zoom: 16,
-        ),
-      ),
-    );
-  }
-
-  //ฟังก์ชัน _saveMapData() มีหน้าที่ เก็บตำแหน่ง ณ ปัจจุบัน ไปไว้ที่ Firebase
-  void _saveMapData() async {
-    var mapData = {
-      "latitude": currentLocation.latitude,
-      "longitude": currentLocation.longitude,
-    };
-
-    // Timer เป็นการกำหนดเวลาให้ส่งค่า latitude และ longitude ทุก ๆ _ วินาที
-    Timer.periodic(const Duration(seconds: 3), (timer) {
-      print(currentLocation);
-      FirebaseFirestore.instance
-          .collection("maps2")
-          .doc("LatLng")
-          // .doc()
-          .set(mapData)
-          .catchError((error) {
-        print("Error saving map data: $error");
-      });
-    });
+        body: StreamBuilder<DocumentSnapshot>(
+          stream: firestore.collection('patient').doc('LatLng').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (!snapshot.hasData) return const Text('Loading...');
+            final double field1Value = snapshot.data!['latitude'] as double;
+            final double field2Value = snapshot.data!['longitude'] as double;
+            print(field1Value);
+            print(field2Value);
+            return Column(
+              children: <Widget>[
+                Text(field1Value.toString()),
+                Text(field2Value.toString()),
+              ],
+            );
+          },
+        ));
   }
 }
